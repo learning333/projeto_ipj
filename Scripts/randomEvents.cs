ac5f93b170fba41ab7587fbe66de793d88ce7628
learning333 <simia.teste@gmail.com>
2021-07-27 12:55:54 -0300

    teste

--- Scripts/randomEvents.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class randomEvents : MonoBehaviour
-{
-	public GameObject[] lista;
-	public float eventosInterval;
-	public int callzero;
-}
--- Scripts/tower.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class tower : MonoBehaviour
-{
-	public int cost;
-	public int health;
-}
